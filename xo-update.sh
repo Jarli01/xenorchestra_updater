b98d377587c8cad07744f34bb5a269a5b0bc41af
Jarli01 <dustinbeuerlein1@gmail.com>
2016-02-06 09:19:51 -0500

    Version check
    Adds a version check to the script so you have a general idea of what version you're on. Assuming Olivier and his team remember to update the dit tags.

--- xo-update.sh
+++ xo-update.sh
@@ -3,11 +3,15 @@
 sudo kill $(ps aux | grep "node bin/xo-server" | grep -v grep | cut -d' ' -f8)
 
 cd /opt/xo-server
+git describe 
+sleep 10s
 sudo git pull
 sudo npm i
 sudo npm run build
 
 cd /opt/xo-web
+git describe
+sleep 10s
 sudo git pull
 sudo npm i
 sudo npm run build
