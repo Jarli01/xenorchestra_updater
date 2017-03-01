ab35e2d0ff7b8497fd3056536d08952041e6c7db
Jarli01 <dustinbeuerlein1@gmail.com>
2017-03-01 15:23:34 -0500

    Supports YARN which is the new dependency module rather than NPM
    NPM has fallen out of graces as the default manager for XOA, we're updating to add YARN support. Additional details will be included on how to install yarn into debian / ubuntu

--- xo-update.sh
+++ xo-update.sh
@@ -28,8 +28,7 @@ if [ $REVISIONS -ne 0 ] || [ "$FORCE" = true ]; then
   echo "Installing..."  
   sed -i 's/< 5/> 0/g' /opt/xo-web/src/xo-app/settings/config/index.js
   sed -i 's/< 5/> 0/g' /opt/xo-web/src/xo-app/xosan/index.js
-  sudo npm i
-  sudo npm run build
+  sudo yarn
   echo Updated version $(git describe --abbrev=0)
 fi
 }
