b5aee69caaa4edf3daa08c7dcec2e235e9c24649
Danp2 <danpollak2@gmail.com>
2017-04-08 10:04:12 -0500

    Inform user of Yarn status

--- xo-update.sh
+++ xo-update.sh
@@ -113,12 +113,14 @@ main() {
 
 updateYarn()
 {
+	
 	if [ $(dpkg-query -W -f='${Status}' yarn 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
 		curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 		echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+	else
 	fi
 
-	sudo apt-get update
+	sudo apt-get update > /dev/null 
 	sudo apt-get install --yes yarn
 }
 
